eca3f708c44de93a318228b3812b44110d8805ef
bluebearhong <bluebearhong@gmail.com>
2017-09-17 03:28:43 +0900

    first commit

---
+++ src/Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public void calculate() {
+        System.out.println(1 + 2);
+        System.out.println(3 - 1);
+        System.out.println(1 - 3);
+        System.out.println(5.1 * 4);
+        System.out.println(6 / 2);
+
+
+        // special case
+        System.out.println(5 / 2);
+        System.out.println(5 % 2);
+    }
+}
---
+++ src/TestMain.java
@@ -0,0 +1,14 @@
+/** 주석 달기
+ *
+ */
+
+public class TestMain {
+    public static void main(String[] args) {
+
+        //        System.out.println("hello");
+
+        //Class 01 operators test
+        Operators operators = new Operators();
+        operators.calculate();
+    }
+}
